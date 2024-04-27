6b85f0dc39b0a5cc07f8d111ab7c1cac239f7dd9
Scott Shealy <vsshealy@gmail.com>
2024-04-26 22:34:47 -0400

    Added theme PHP files

---
+++ php/scripts/external/google-analytics/header.php
@@ -0,0 +1,9 @@
+<?php 
+    /**
+     * php/scripts/external/google-analytics/header.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2024.04.26)
+     * @copyright 2024 (2024.04.26)
+    **/
+?>
